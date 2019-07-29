eb2e279d137021c5e6292a3ebd210b640751c3a0
Park June Chul <pjc0247@naver.com>
2019-07-29 17:03:49 +0900

    init

---
+++ Pathscript/Pathscript.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Pathscript
+{
+    public class PScript
+    {
+        public static object Eval(object context, string src)
+        {
+            if (string.IsNullOrEmpty(src))
+                throw new ArgumentException(nameof(src));
+
+            var asm = context.GetType().Assembly;
+            var tokens = src.Split('.');
+            var _this = context;
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                var isLastToken = i == tokens.Length - 1;
+                if (isLastToken)
+                {
+                    return GetPropertyOrField(_this, tokens[i]);
+                }
+
+                var type = asm.GetTypes()
+                    .Where(x => x.Name == tokens[i])
+                    .FirstOrDefault();
+                if (type != null)
+                    _this = type;
+                else
+                    _this = GetPropertyOrField(_this, tokens[i]);
+            }
+
+            return null;
+        }
+
+        private static object GetStaticPropertyOrField(Type type, string key)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var field = type.GetField(key, BindingFlags.Static | BindingFlags.Public);
+            if (field != null)
+                return field.GetValue(null);
+
+            var property = type.GetProperty(key, BindingFlags.Static | BindingFlags.Public);
+            if (property != null)
+                return property.GetValue(null);
+
+            throw new InvalidOperationException($"{key} does not exist in {type}.");
+        }
+        private static object GetInstancePropertyOrField(object obj, string key)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            var type = obj.GetType();
+            var field = type.GetField(key, BindingFlags.Instance | BindingFlags.Public);
+            if (field != null)
+                return field.GetValue(obj);
+
+            var property = type.GetProperty(key, BindingFlags.Instance | BindingFlags.Public);
+            if (property != null)
+                return property.GetValue(obj);
+
+            throw new InvalidOperationException($"{key} does not exist in {obj}.");
+        }
+        private static object GetPropertyOrField(object obj, string key)
+        {
+            if (obj is Type t)
+                return GetStaticPropertyOrField(t, key);
+            else
+                return GetInstancePropertyOrField(obj, key);
+        }
+    }
+}
---
+++ Pathscript/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathscript
+{
+    class Foo
+    {
+        public static int bar = 1234;
+        public int foo = 123;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Foo();
+
+            Console.WriteLine(PScript.Eval(f, "Foo.bar"));
+        }
+    }
+}
