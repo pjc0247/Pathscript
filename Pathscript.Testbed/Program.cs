ffac6674cd85feea83a55ea4c970dce7503709e2
Park June Chul <pjc0247@naver.com>
2019-08-01 10:18:34 +0900

    update

---
+++ Pathscript.Testbed/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathscript.Testbed
+{
+    class Foo
+    {
+        public int[] a = new int[] { 1, 2, 3, 4 };
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Foo();
+
+            Console.WriteLine(PScript.Eval(f, "a[1]"));
+        }
+    }
+}
--- Pathscript/Pathscript.cs
+++ Pathscript/Pathscript.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Pathscript
 {
+    enum LiteralType
+    {
+        Integer,
+        Character,
+        String
+    }
+
     public class PScript
     {
+        private static Regex regArray = new Regex("\\[([0-9+])\\]");
+
         public static object Eval(object context, string src)
         {
             if (string.IsNullOrEmpty(src))
@@ -19,22 +29,76 @@ namespace Pathscript
             var _this = context;
             for (int i = 0; i < tokens.Length; i++)
             {
+                var token = tokens[i];
+                var matches = regArray.Matches(token);
+                object result = null;
+
+                if (matches.Count > 0)
+                    token = token.Split(new char[] { '[' }, 2)[0];
+
                 var isLastToken = i == tokens.Length - 1;
                 if (isLastToken)
                 {
-                    return GetPropertyOrField(_this, tokens[i]);
+                    result = GetPropertyOrField(_this, token);
+                }
+                else
+                {
+                    var type = asm.GetTypes()
+                        .Where(x => x.Name == token)
+                        .FirstOrDefault();
+                    if (type != null)
+                        result = type;
+                    else
+                        result = GetPropertyOrField(_this, token);
                 }
 
-                var type = asm.GetTypes()
-                    .Where(x => x.Name == tokens[i])
-                    .FirstOrDefault();
-                if (type != null)
-                    _this = type;
+                foreach (Match match in matches)
+                {
+                    var tp = ParseLiteral(match.Groups[1].Value);
+                    var type = tp.Item1;
+                    var idx = tp.Item2;
+
+                    if (result is Array ary)
+                        result = ary.GetValue((int)idx);
+                    else
+                    {
+                        foreach (var pi in result.GetType().GetProperties())
+                        {
+                            if (pi.GetIndexParameters().Length > 0)
+                            {
+                                result = pi.GetValue(result, new object[] { idx });
+                            }
+                        }
+                    }
+                }
+
+                if (isLastToken)
+                    return result;
                 else
-                    _this = GetPropertyOrField(_this, tokens[i]);
+                    _this = result;
             }
 
             return null;
+        }
+
+        private static Tuple<LiteralType, object> ParseLiteral(string str)
+        {
+            if (str.Length > 2 && str.First() == '"' && str.Last() == '"')
+            {
+                return new Tuple<LiteralType, object>(
+                    LiteralType.String, str.Substring(1, str.Length - 2));
+            }
+            if (str.Length > 2 && str.First() == '\'' && str.Last() == '\'')
+            {
+                return new Tuple<LiteralType, object>(
+                    LiteralType.Character, str[1]);
+            }
+
+            int n;
+            if (int.TryParse(str, out n))
+                return new Tuple<LiteralType, object>(LiteralType.Integer, n);
+
+            throw new ArgumentException("Unrecognized literal: " + str);
         }
 
         private static object GetStaticPropertyOrField(Type type, string key)
