8d703d432f670417260caed04a0228fd53694681
Park June Chul <pjc0247@naver.com>
2019-12-27 15:39:41 +0900

    add PathscriptExt

---
+++ Pathscript/PathscriptExt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathscript
+{
+    public static class PathscriptExt
+    {
+        public static object Eval(this object _this, string script)
+            => PScript.Eval(_this, script);
+        public static T Eval<T>(this object _this, string script)
+            => (T)PScript.Eval(_this, script);
+    }
+}
